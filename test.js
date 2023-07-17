79343beaf07fd7afb53d98f9f7ebce51bcd5893e
mohammadrjoub1 <mohammadrjoub7@gmail.com>
2023-07-17 18:47:54 +0300

    dfgd
    f

---
+++ test.js
@@ -0,0 +1,14 @@
+﻿let fighters = ["🐉", "🐥", "🐊", "💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷", "🐆", "🦕", "🦁"]
+
+let stageEl = document.getElementById("stage")
+let fightButton = document.getElementById("fightButton")
+
+
+function cliked() {
+
+    let x = Math.floor(Math.random() * fighters.length)
+    let y = Math.floor(Math.random() * fighters.length)
+
+    document.getElementById("stage").innerText = fighters[x] + "VS" + fighters[y];
+
+}
